21c48b0033759004e82dfc7c8a2948a252afb012
Gether1996 <pat.kredatus@gmail.com>
2023-03-13 21:53:24 +0100

    reworked view of homepage to filter most urgent note, small adjustments to HTMLs

--- viewer/views.py
+++ viewer/views.py
@@ -1,11 +1,30 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
+from datetime import datetime, timezone, timedelta
 from viewer.forms import CategoryForm, NoteForm
 from viewer.models import *
 
 
 def homepage(request):
-    return render(request, 'homepage.html')
+    user_id = request.user.id
+    now_utc = datetime.now(timezone.utc)
+    notes_ordered = Note.objects.filter(user_id=user_id, scheduled_time__gte=now_utc).order_by('scheduled_time')
+    first_note = notes_ordered.first()
+    if first_note is not None:
+        remaining_time = (first_note.scheduled_time - now_utc).total_seconds()
+        days = int(remaining_time // (24 * 3600))
+        seconds = int(remaining_time % (24 * 3600))
+        hours = seconds // 3600
+        minutes = (seconds // 60) % 60
+        seconds = seconds % 60
+
+        if days > 0:
+            remaining_time_formatted = f"{days}d:{hours:02d}h:{minutes:02d}m:{seconds:02d}s"
+        else:
+            remaining_time_formatted = f"{hours:02d}h:{minutes:02d}m:{seconds:02d}s"
+    else:
+        remaining_time_formatted = ''
+    return render(request, 'homepage.html', {"first_note": first_note, "remaining_time_formatted": remaining_time_formatted})
 
 
 @login_required
